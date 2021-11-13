c8ad398f5510a63a079d9dc907ee2350b95caf2d
Tch1b0 <Jpour3006@gmail.com>
2021-11-13 13:26:09 +0100

    add intro folder

--- scripts/InitSettings.gd
+++ scripts/InitSettings.gd
@@ -2,18 +2,18 @@ extends Settings
 
 # Initialize the settings
 func _enter_tree():
-    auto_load = true
-    auto_save = true
+	auto_load = true
+	auto_save = true
 
-    set_default("volume", 1.0)
-    set_default("music", true)
-    set_default("fullscreen", false)
+	set_default("volume", 1.0)
+	set_default("music", true)
+	set_default("fullscreen", false)
 
-    adapt_settings()
+	adapt_settings()
 
 # Adapt the settings
 # For e.g. set the fullscreen
 func adapt_settings():
-    # Set fullscreen to the setting 'fullscreen'
-    # Default is false
-    OS.window_fullscreen = gets("fullscreen")
+	# Set fullscreen to the setting 'fullscreen'
+	# Default is false
+	OS.window_fullscreen = gets("fullscreen")
