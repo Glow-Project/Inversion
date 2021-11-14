3e13fe63c2ae9ecead9ca53b77050c9b004a2d7c
Tch1b0 <Jpour3006@gmail.com>
2021-11-14 11:06:32 +0100

    Finish intro animation

--- game/Game.gd
+++ game/Game.gd
@@ -1,13 +1,21 @@
 extends Control
 
-func _on_PatentsButton_pressed():
+func _ready():
+	# Start the diashow in the background
+	$Background.visible = true
+	$Background/AnimationPlayer.play("diashow")
+	
+	$Music.play(global.audio_timestamps["bahamas"])
+	$AnimationPlayer.play("enter")
+
+func _on_Patents_pressed():
 	get_tree().change_scene("res://game/apps/patent/PatentApp.tscn")
 
-func _on_NewsButton_pressed():
+func _on_News_pressed():
 	get_tree().change_scene("res://game/apps/news/NewsApp.tscn")
 
-func _on_LawsuitButton_pressed():
+func _on_Lawsuits_pressed():
 	get_tree().change_scene("res://game/apps/lawsuit/LawsuitApp.tscn")
 
-func _on_BankButton_pressed():
+func _on_Bank_pressed():
 	get_tree().change_scene("res://game/apps/bank/BankApp.tscn")
