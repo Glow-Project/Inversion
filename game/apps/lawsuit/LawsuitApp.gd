58714d39212645f1f2280259d99bd0a9e3b5d730
Raphael Pour <info@raphaelpour.de>
2021-11-17 20:35:30 +0100

    Lawsuit App: IMprove ui

--- game/apps/lawsuit/LawsuitApp.gd
+++ game/apps/lawsuit/LawsuitApp.gd
@@ -11,14 +11,13 @@ func sync_state():
 	$VisibleInvention/Description.text = current_invention.description
 	$VisibleInvention/Draft.texture = current_invention.draft
 
-	$SelectPatent.clear()
-	for patent in patent_collection.get_owned():
+	$PatentList.clear()
+	for patent in patent_collection.get_all():
 		print(str(patent))
-		$SelectPatent.add_item(str(patent))
+		$PatentList.add_item(str(patent))
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://game/Game.tscn")
 
-
-func _on_SelectPatent_item_selected(index):
-	selected_patent = patent_collection.get_owned()[index]
+func _on_PatentList_item_selected(index):
+	selected_patent = patent_collection.get_all()[index]
