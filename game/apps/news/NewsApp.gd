f91898c074cd460a8dcff2813a055b78aa6bca68
Raphael Pour <info@raphaelpour.de>
2021-11-19 21:05:05 +0100

    News: add random inventions

--- game/apps/news/NewsApp.gd
+++ game/apps/news/NewsApp.gd
@@ -1,7 +1,17 @@
 extends Control
 
 func _ready():
-	pass
+	var inventions = invention_collection.get_random(2)
+	
+	if len(inventions) != 2:
+		print("Inventions array has only %d items" % [len(inventions)])
+		return
+	
+	$Invention_1.texture = inventions[0].draft
+	$Invention_1/InventionLabel1.text = inventions[0].name
+	
+	$Invention_2.texture = inventions[1].draft
+	$Invention_2/InventionLabel2.text = inventions[1].name
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://game/Game.tscn")
