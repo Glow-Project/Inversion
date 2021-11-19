f01bf6b5fe6abdfd93a8892123e3a6c0d36967f6
Raphael Pour <info@raphaelpour.de>
2021-11-19 20:36:16 +0100

    News: Implement app

--- game/apps/news/NewsApp.gd
+++ game/apps/news/NewsApp.gd
@@ -1,4 +1,13 @@
 extends Control
 
-func _on_Button_pressed():
+func _ready():
+	pass
+
+func _on_Back_pressed():
 	get_tree().change_scene("res://game/Game.tscn")
+
+func _on_SelectButton1_pressed():
+	pass
+
+func _on_Sue2_pressed():
+	pass
