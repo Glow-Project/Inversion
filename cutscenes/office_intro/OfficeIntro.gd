1c6bb08fc60569f13540531a17ca3740eae08e0c
Tch1b0 <Jpour3006@gmail.com>
2021-11-21 18:23:42 +0100

    fix intro

--- cutscenes/office_intro/OfficeIntro.gd
+++ cutscenes/office_intro/OfficeIntro.gd
@@ -3,6 +3,10 @@ extends Node2D
 func _ready():
 	$AnimationPlayer.play("office_intro")
 
+func _process(_delta):
+	if Input.is_action_just_pressed("skip"):
+		get_tree().change_scene("res://game/Game.tscn")
+
 func kill_music():
 	$ContinuousMusic.queue_free()
 
