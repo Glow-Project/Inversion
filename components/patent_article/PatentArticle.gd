ee198ce919209a186460f82f0df106e83fb01ca2
Raphael Pour <info@raphaelpour.de>
2021-11-21 13:03:08 +0100

    Shop: Hide button if owned, disable button if not enough money

--- components/patent_article/PatentArticle.gd
+++ components/patent_article/PatentArticle.gd
@@ -7,9 +7,13 @@ signal bought(patent)
 func _ready():
 	$Draft.texture = patent.draft
 	$Name.text = patent.name
-	$Price.text = "%d USD" % [patent.price]
-	$BuyButton.disabled = patent.owned
-
+	
+	if patent.owned:
+		$Price.text = "OWNED"
+		$BuyButton.hide()
+	else:
+		$Price.text = "%d USD" % [patent.price]
+		$BuyButton.disabled = patent.price > bank_account.current_money
 
 func _on_BuyButton_pressed():
 	if patent.buy():
