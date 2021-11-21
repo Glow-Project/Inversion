2e2e3f396cee557440ef0eb24528b1cdc7908db8
Raphael Pour <info@raphaelpour.de>
2021-11-21 17:32:42 +0100

    LawsuitApp: Disable sue button on patent/invention mismatch

--- game/apps/lawsuit/LawsuitApp.gd
+++ game/apps/lawsuit/LawsuitApp.gd
@@ -9,6 +9,9 @@ func _ready():
 	for i in range(len(patents)):
 		$PatentList.add_item(str(patents[i]))
 		$PatentList.set_item_tooltip_enabled(i,false)
+		
+	$PatentList.select(0,true)
+	_on_PatentList_item_selected(0)
 	sync_state()
 
 func sync_state():
@@ -30,6 +33,8 @@ func _on_PatentList_item_selected(index):
 	selected_patent = patent_collection.get_owned()[index]
 
 	print(selected_patent.price)
+	
+	$Sue.disabled = selected_patent.id != current_invention.patent_id
 
 	global.win_money_amount = int(selected_patent.price * 2)
 	global.lose_money_amount = int(global.win_money_amount * 0.5)
@@ -37,14 +42,13 @@ func _on_PatentList_item_selected(index):
 
 
 func _on_Sue_pressed():
-	if selected_patent == null:
-		# Place UI warning here
-		printerr("Oh no! Nothing is selected")
-	elif selected_patent.id == current_invention.patent_id:
-		# When everything is ok
-		global.remove_invention(current_invention)
-		global.selected_patent = selected_patent
-		global.selected_invention = current_invention
-		get_tree().change_scene("res://cutscenes/court/Court.tscn")
-	else:
-		printerr("Oh no!!! This does not match :((((((")
+	if selected_patent == null || current_invention == null:
+		return
+		
+	if selected_patent.id != current_invention.patent_id:
+		return
+		
+	global.remove_invention(current_invention)
+	global.selected_patent = selected_patent
+	global.selected_invention = current_invention
+	get_tree().change_scene("res://cutscenes/court/Court.tscn")
